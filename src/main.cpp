f81a33a553c7e068d5a4263c67abd27b5a8b46dc
ziqiangao <88281267+ziqiangao@users.noreply.github.com>
2025-02-20 12:50:04 +0000

    Some Formatting + Debug

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <filesystem> // C++17 for filesystem handling
 #include <Geode/binding/GJSongBrowser.hpp>
 #include <Geode/ui/BasedButtonSprite.hpp>
+#include <Geode/binding/DialogObject.hpp>
 
 void showSSMenu()
 {
@@ -76,21 +77,45 @@ void createDirectoryIfNeeded(const std::string &path)
         geode::log::info("Created directory: {}", directoryPath);
     }
 }
-
+int i = 0;
 class $modify(MenuLayer)
 {
     void onMoreGames(CCObject *target)
     {
         showSSMenu();
-    };
+    }
+
+    void onRobTop(CCObject *sender)
+    {
+        switch (i) {
+        case 0:
+            CCDirector::sharedDirector()->getRunningScene()->addChild(DialogLayer::create(
+                DialogObject::create("Debug (Song Path)", p->pathForSong(0), 2, 1.0f, true, ccWHITE), 2
+            ));
+            i = 1;
+            break;
+            
+        case 1:
+            CCDirector::sharedDirector()->getRunningScene()->addChild(DialogLayer::create(
+                DialogObject::create("Debug (SFX Path)", p->pathForSFX(0), 4, 1.0f, true,ccWHITE), 2
+            ));
+            i = 2;
+            break;
+        case 2:
+            CCDirector::sharedDirector()->getRunningScene()->addChild(DialogLayer::create(
+                DialogObject::create("Key Master", "What are doing here!?!?", 18, 1.0f, true,ccWHITE), 2
+            ));
+            i = 0;
+            break;
+        }
+    }
 };
+
 
 class $modify(MusicDownloadManager)
 {
     gd::string pathForSong(int p0)
     {
-        geode::log::debug("pathForSong");
-
         std::string fullPath = std::string(folder);
         if (!fullPath.empty() && (fullPath.back() == '\\' || fullPath.back() == '/'))
         {
@@ -105,7 +130,7 @@ class $modify(MusicDownloadManager)
 
         // Ensure directories exist
         createDirectoryIfNeeded(fullPath);
-
+        geode::log::debug("pathForSong {}", fullPath);
         return gd::string(fullPath);
     }
 
@@ -129,20 +154,22 @@ class $modify(MusicDownloadManager)
 
     bool isSongDownloaded(int p0)
     {
-        log::debug("Checked Song {}", std::to_string(p0));
+
         std::string songPath = std::string(pathForSong(p0)); // Convert gd::string to std::string
 
         // Check if the file exists in the specified path
         if (std::filesystem::exists(songPath))
         {
+            log::debug("Checked Song {} Exists", std::to_string(p0));
             return true;
         }
-
+        log::debug("Checked Song {} Not Downloaded", std::to_string(p0));
         return false;
     }
-    
+
     static MusicDownloadManager *sharedState()
     {
+        log::info("Cleared Resource IDS");
         auto p = MusicDownloadManager::sharedState();
         p->m_resourceSfxUnorderedSet.clear();
         p->m_resourceSongUnorderedSet.clear();
